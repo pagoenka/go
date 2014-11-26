82313b1a42ec1f131fec17cf2d4936db7aac3aeb
Pratik Goenka <pratik@mac-pro90.synerzipune.local>
2014-11-26 11:32:13 +0530

    running prog

--- src/pratik/t9.go
+++ src/pratik/t9.go
@@ -5,37 +5,39 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 
-	var t9map map[string]int
-	t9map["a"] = 2
-	t9map["b"] = 22
-	t9map["c"] = 222
-	t9map["d"] = 3
-	t9map["e"] = 33
-	t9map["f"] = 333
-	t9map["g"] = 4
-	t9map["h"] = 44
-	t9map["i"] = 444
-	t9map["j"] = 5
-	t9map["k"] = 55
-	t9map["l"] = 555
-	t9map["m"] = 6
-	t9map["n"] = 66
-	t9map["o"] = 666
-	t9map["p"] = 7
-	t9map["q"] = 77
-	t9map["r"] = 777
-	t9map["s"] = 7777
-	t9map["t"] = 8
-	t9map["u"] = 88
-	t9map["v"] = 888
-	t9map["w"] = 9
-	t9map["x"] = 99
-	t9map["y"] = 999
-	t9map["z"] = 9999
+	t9map := map[string]int{
+		"a": 2,
+		"b": 22,
+		"c": 222,
+		"d": 3,
+		"e": 33,
+		"f": 333,
+		"g": 4,
+		"h": 44,
+		"i": 444,
+		"j": 5,
+		"k": 55,
+		"l": 555,
+		"m": 6,
+		"n": 66,
+		"o": 666,
+		"p": 7,
+		"q": 77,
+		"r": 777,
+		"s": 7777,
+		"t": 8,
+		"u": 88,
+		"v": 888,
+		"w": 9,
+		"x": 99,
+		"y": 999,
+		"z": 9999,
+	}
 
 	fileHandle, _ := os.Open("cs.txt")
 	defer fileHandle.Close()
@@ -47,11 +49,12 @@ func main() {
 	var itr int64
 
 	for itr = 1; itr <= numCases; itr++ {
+		fmt.Print("Case #", itr, ": ")
 		fileScanner.Scan()
-		byteArray := []byte(fileScanner.Text())
-		for idx, str := range byteArray {
-			fmt.Printf(string(str))
-			fmt.Printf(string(idx))
+		arrStrings := strings.Split(fileScanner.Text(), "")
+		for str := range arrStrings {
+			fmt.Print(t9map[arrStrings[str]])
 		}
+		fmt.Println("")
 	}
 }
